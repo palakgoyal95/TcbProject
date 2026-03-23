194dea87e59b49544a65ded322a22f983185672d
palakgoyal95 <palakgoyal29062002@gmail.com>
2026-03-23 22:40:01 +0530

    fix: Update .env to remove unneces

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -172,6 +172,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Internal observability token used by /metrics endpoints.
 METRICS_TOKEN = os.getenv("METRICS_TOKEN", "")
